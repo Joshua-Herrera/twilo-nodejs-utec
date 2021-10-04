fbbb40c247c6fe78bc7d916009acae7d022091a2
Joshua-Herrera <91395285+Joshua-Herrera@users.noreply.github.com>
2021-10-04 09:39:27 -0600

    1rst load

---
+++ app.js
@@ -0,0 +1,41 @@
+const accountSid = 'ACd284e813eeae6da68319666a962f47d3';
+const authToken = '44c876cbecb0d9497bae3b18987f204b';
+const client = require('twilio')(accountSid, authToken);
+const express = require('express');
+
+const app = express()
+
+app.use(express.json())
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-credentials', 'true');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, DELETE, PATCH');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Authorization, Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+app.post('/', (req, res) => {
+    // console.log(req.body)
+    client.messages
+    .create({
+        body: req.body.msg,
+        messagingServiceSid: 'MGea76f5e9e32111b1a302b97e121e5360',
+        to: req.body.tn
+    })
+    // .then(message => console.log(message.sid))
+    .done();
+})
+
+
+app.listen(3000, () => {
+    console.log(`API REST is running in port 3000 !!!`)
+})
+
